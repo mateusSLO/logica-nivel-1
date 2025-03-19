8d072efda53ba4d1271b9ae6fce3393f95c7cc5d
mateusSLO <mateusrj95@gmail.com>
2025-03-18 21:53:29 -0300

    Create 10.py

---
+++ 10.py
@@ -0,0 +1,12 @@
+print("Digite a primreira nota:")
+numero1 = int(input())
+print("Digite a sgunda nota:")
+numero2 = int(input())
+print("Digite a terceira nota:")
+numero3 = int(input())
+print("Digite a quarta nota:")
+numero4 = int(input())
+media1 = numero1+numero2+numero3+numero4
+mediaf = media1/4
+print("Sua média é:")
+print(mediaf)
