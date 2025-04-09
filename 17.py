80091ae3872cc69d71b746002739f39540d8adf6
mateusSLO <mateusrj95@gmail.com>
2025-04-08 21:17:32 -0300

    Create 17.py

---
+++ 17.py
@@ -0,0 +1,13 @@
+print("Informe o primeiro número")
+num1 = float(input())
+print("Informe o segundo número")
+num2 = float(input())
+print("Informe o terceiro número")
+num3 = float(input())
+if num1 > num2 and num1 > num3:
+    num4 = num1
+elif num2 > num3 and num2 > num1:
+    num4 = num2
+elif num3 > num2 and num3 > num1:
+    num4 = num3
+print("O maior numero é:", num4)
